13c223e38c4b48de109421685583d841f2bceaa6
Alfredo Urdaneta <alfredourdaneta@Alfredos-MBP.attlocal.net>
2024-04-23 15:15:35 -0400

    Added graphql schemas and types

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
-import {
-  Authenticated,
-  GitHubBanner,
-  Refine,
-  WelcomePage,
-} from "@refinedev/core";
+import { Authenticated, GitHubBanner, Refine } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
@@ -20,7 +15,7 @@ import routerBindings, {
 import { App as AntdApp } from "antd";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import { Home, ForgotPassword, Login, Register } from "./pages";
-import Layout from "./components/layout";
+import { Layout } from "./components/layout";
 
 function App() {
   return (
--- src/components/custom-avatar/index.tsx
+++ src/components/custom-avatar/index.tsx
@@ -1,6 +1,38 @@
-const CustomAvatar = () => {
+import React from "react";
+
+import type { AvatarProps } from "antd";
+import { Avatar as AntdAvatar } from "antd";
+
+import { getNameInitials, getRandomColorFromString } from "@/utilities";
+
+type Props = AvatarProps & {
+  name?: string;
+};
+
+const CustomAvatarComponent = ({ name = "", style, ...rest }: Props) => {
   return (
-    <div>CustomAvatar</div>
-  )
-}
-export default CustomAvatar
+    <AntdAvatar
+      alt={name}
+      size="small"
+      style={{
+        backgroundColor: rest?.src
+          ? "transparent"
+          : getRandomColorFromString(name),
+        display: "flex",
+        alignItems: "center",
+        border: "none",
+        ...style,
+      }}
+      {...rest}
+    >
+      {getNameInitials(name)}
+    </AntdAvatar>
+  );
+};
+
+export const CustomAvatar = React.memo(
+  CustomAvatarComponent,
+  (prevProps, nextProps) => {
+    return prevProps.name === nextProps.name && prevProps.src === nextProps.src;
+  },
+);
