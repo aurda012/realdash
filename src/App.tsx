258d0163d25b08fea3cda2e9476169d9e6faaf81
Alfredo Urdaneta <alfredourdaneta@Alfredos-MBP.attlocal.net>
2024-04-23 16:40:39 -0400

    Added resources config and companies list

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,8 @@ import { App as AntdApp } from "antd";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import { Home, ForgotPassword, Login, Register } from "./pages";
 import { Layout } from "./components/layout";
+import { CompaniesList } from "./pages/companies/list";
+import { resources } from "./config/resources";
 
 function App() {
   return (
@@ -30,6 +32,7 @@ function App() {
               notificationProvider={useNotificationProvider}
               routerProvider={routerBindings}
               authProvider={authProvider}
+              resources={resources}
               options={{
                 syncWithLocation: true,
                 warnWhenUnsavedChanges: true,
@@ -55,6 +58,11 @@ function App() {
                   }
                 >
                   <Route index element={<Home />} />
+                  <Route path="/companies">
+                    <Route index element={<CompaniesList />} />
+                    {/* <Route path="new" element={<Create />} />
+                        <Route path="edit/:id" element={<Edit />} /> */}
+                  </Route>
                 </Route>
               </Routes>
               <RefineKbar />
